7fa3a996ed85a19b8582bc99a00fde6def0c5f13
circuitapps <125308597+circuitapps@users.noreply.github.com>
2023-10-14 22:36:13 -0700

    Completed the first draft
    - Documentation updated.
    - Circuit diagram added
    - Working ATtiny85 code added
    - Project's youtube link still to be added

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # 23118_windless_windchime
-Collaterals for circuitapps' windless windchimes project
+
+---
+This is one of the coolest projects we undertook up to date. It is about transforming an off-the-shelf table top windchime that has a motor fan into a magnetically driven one! The main advantage of this transformation is the fan motor noise will be completely eliminated thereby making the windchime very quiet. Another great thing about the change we add is it brings a certain level of magic into the equation since the windchime appears to be activated without any visible force. Watch our **[watch our construction video using this link][1]** and how the magical operation appears!
+
+Although we worked on a specific type of windchime (bought from a charity store!), the methods can easily be adapted to any windchime that is on a stand. The circuit diagram and the microcontroller code can be used directly without changing anything. You can even adapt and change the code if you like. Experimenting and trying new things is always a good thing!
+
+Following is the working circuit diagram you will need if you want to build this circuit. Notice we have included the coil that you will need in the circuit diagram as well for convenience. If you prefer to use other types of coils, please experiment and pick the one that works best for you.
+
+![circuit diagram](./ATtiny85_coil_control_circuit.png)
+
+The circuit has been designed to work with 5V DC and therefore you can power this using an ordinary USB power bank. What this means is you can take your windchime anywhere you like. (The original product required a 6V power supply, which we had to discard as part of the modifications we made)
+
+If you build this project, please share your thoughts and suggestions with the rest of circuitapps community in the comments section of **[our YouTube video][1]**. Also, please feel free to talk about any interesting modifications you make and your experimentations, if any !
+
+## Project Challenges
+There are a few things to be careful with while building this project to make sure you get it working after the first attempt
+
+### **Caution 1 - Coil polarity and the magnet**
+
+We added a disc shaped strong magnet to interact with the coil that generates a variable electromagnetic force as controlled by the microcontroller in our circuit (i.e, ATtiny85) You may need to flip the polarity of the coil of the coil does not push the magnet. This is a simple change you may need to make during the testing phase.
+
+### **Caution 2 - Using the three switches**
+
+The original windchime came with a 3-switch module, which we modified and continued using. If you use an ordinary windchime in your project, you will need to add three switches yourself. Alternatively, you may want to discard the switches altogether, which will also work. If you discard the switches, be sure to modify the microcontroller code to skip the power ON/OFF routine.
+
+## Useful tips
+
+### **Tip 1:**
+If you have not worked with ATtiny85 before and need support with the basic operation and programming of this device, have a look at this **[excellent reference][2]** that walks you through the entire process step by step. If you get stuck, drop us a message in the comments section of **[our YouTube video][1]**
+
+
+GOOD LUCK & ENJOY BUILDING THIS COOL PROJECT !
+
+[1]: ADD YOU TUBE PROJECT LINK HERE https://youtu.be/lHvzVke-1bM
+
+[2]: https://circuitdigest.com/microcontroller-projects/programming-attiny85-microcontroller-ic-using-arduino 
--- attiny85_code/attiny85_code.ino
+++ attiny85_code/attiny85_code.ino
@@ -1,34 +1,53 @@
+
 /*
-  Blink
+  Windless windchime control code
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+  Operation: ON_OFF_PIN turns the device on if it is off and vice versa. While the device is off, LEVEL_UP_PIN and LEVEL_DOWN_PIN presses are ignored.
+  When the device is turned on, the COIL_OUT_PIN is activated every DEFAULT_PULSE_RATE x TIME_BASE_MSEC milliseconds. To make the coil activation
+  more frequent, user needs to press the LEVEL_UP_PIN. To reduce the coil activation rate, user needs to press the LEVEL_DOWN_PIN. Each button press 
+  increases or descreases the coil activation rate by TIME_BASE_MSEC milliseconds. The slowest coil activation rate is MIN_COIL_PULSE_RATE x TIME_BASE_MSEC milliseconds.
+  The fastest coil activation rate is MAX_COIL_PULSE_RATE x TIME_BASE_MSEC milliseconds.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+  Each time any of the ON_OFF_PIN, LEVEL_UP_PIN or LEVEL_DOWN_PINs are pressed the LED_OUT_PIN goes high twice; one for the rising edge and one for the falling edge as
+  a result of mechanically pressing and depressing the pushbutton. All the pin interactions are interrupt driven.
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+  by circuitapps
+  October 2023
 */
+#include <avr/io.h>
 
-#define PIN_0 0
-#define PIN_1 1
-#define PIN_2 2
-#define PIN_3 3
-#define PIN_4 4
+// PORT B DEFINITIONS
+#define PB5_PIN1 PB5
+#define PB4_PIN3 PB4
+#define PB3_PIN2 PB3
+#define PB2_PIN7 PB2
+#define PB1_PIN6 PB1
+#define PB0_PIN5 PB0
 
-#define MAX_REPS 20
+// Application pin definitions
+#define COIL_OUT_PIN PB3_PIN2
+#define LED_OUT_PIN PB0_PIN5
+#define LEVEL_UP_PIN PB1_PIN6
+#define LEVEL_DOWN_PIN PB4_PIN3
+#define ON_OFF_PIN PB2_PIN7
+
+#define PC_INT_VECTOR PCINT0_vect  // On ATtiny85, there is ONE interrupt vector (i.e., one interrupt service routine) that gets called when ANY of the ctive interrupt pins are triggered !
+
+//#define MAX_REPS 20
+#define MAX_COIL_PULSE_RATE 1  // Pulse once every second
+#define MIN_COIL_PULSE_RATE 10  // Pulse once every ten seconds
+#define TIME_BASE_MSEC 1000  // Maximum time delay (in milliseconds) before the coil can be reactivated
+#define DEFAULT_PULSE_RATE 5  // initialization value at power up
+
+#define PULSE_DRIVE_ON_PERIOD_MSEC 25  // How long the COIL_OUT_PIN is kept HIGH in milliseconds
+#define PULSE_DRIVE_OFF_PERIOD_MSEC 25  // How long the COIL_OUT_PIN is kept LOW in milliseconds
+
+#define POWER_ON 1
+#define POWER_OFF 0
+
+// Global variables follow
+int current_pulse_rate = 5;  // default coil activation rate is once per 5 seconds
+int current_on_off_state = POWER_OFF;
 
 void pulse_drive(int output_pin, int on_duration_ms, int off_duration_ms)
 {
@@ -38,77 +57,121 @@ void pulse_drive(int output_pin, int on_duration_ms, int off_duration_ms)
   delay(off_duration_ms);          // wait
 }
 
-void pulse_sequence_feature()
+void pin_interrupt_test_setup()
 {
+  int current_pulse_rate = 5;  // default coil activation rate is once per 5 seconds
+  current_on_off_state = POWER_OFF;
 
-  int i;
-
-  pulse_drive(PIN_0, 50, 50);
-
-  for(i=0;i<MAX_REPS; ++i)
-  {
-    pulse_drive(PIN_3, 200, 100);
-  }
-
-  pulse_drive(PIN_0, 50, 50);
-
-  for(i=0;i<MAX_REPS; ++i)
-  {
-    pulse_drive(PIN_3, 100, 100);
-  }
-
-  pulse_drive(PIN_0, 50, 50);
-
-  for(i=0;i<MAX_REPS; ++i)
-  {
-    pulse_drive(PIN_3, 50, 100);
-  }
-
-  pulse_drive(PIN_0, 50, 50);
-
-  for(i=0;i<MAX_REPS; ++i)
-  {
-    pulse_drive(PIN_3, 25, 100);
-  }
-
-  pulse_drive(PIN_0, 50, 50);
-
-  for(i=0;i<MAX_REPS; ++i)
-  {
-    pulse_drive(PIN_3, 10, 100);
-  }
-
+  cli();  // disable interrupts during set up
+  // Three pin interrupts are enabled below
+  PCMSK |= (1 << LEVEL_UP_PIN) | (1 << LEVEL_DOWN_PIN) | (1 << ON_OFF_PIN);
+  GIFR  |= (1 << PCIF);  // clear any outstanding interrupts
+  GIMSK |= (1 << PCIE);  // Pin Change Interrupts are Enabled
+  pinMode(LEVEL_UP_PIN, INPUT_PULLUP);  // 20K pullup enabled. Pin needs to operate in "active low"
+  pinMode(LEVEL_DOWN_PIN, INPUT_PULLUP);  // 20K pullup enabled. Pin needs to operate in "active low"
+  pinMode(ON_OFF_PIN, INPUT_PULLUP);  // 20K pullup enabled. Pin needs to operate in "active low"
+  pinMode(LED_OUT_PIN, OUTPUT);
+  pinMode(COIL_OUT_PIN, OUTPUT);
+  sei();  // enable interrupts after set up (last line of set up)
 }
 
 
-void windless_chime_feature()
+ISR(PC_INT_VECTOR)
+{
+  // One of the interrupt pins got triggered. Identify which.
+
+  // Turn LED on at the start
+  digitalWrite(LED_OUT_PIN, HIGH);  // turn pin on (HIGH is the voltage level)
+
+  if(digitalRead(LEVEL_UP_PIN) == LOW)
+  {
+    if(current_on_off_state == POWER_ON)
+    {
+      // This interrupt will work ONLY IF power is ON
+      if(current_pulse_rate > MAX_COIL_PULSE_RATE)
+      {
+        // Pulse rate not at fastest rate yet 
+        --current_pulse_rate;  // pulse one second faster
+      }
+    }
+    else
+      current_pulse_rate = DEFAULT_PULSE_RATE;
+
+  }
+  else if(digitalRead(LEVEL_DOWN_PIN) == LOW)
+  {
+      if(current_on_off_state == POWER_ON)
+      {
+        // This interrupt will work ONLY IF power is ON
+        if(current_pulse_rate < MIN_COIL_PULSE_RATE)
+        {
+          // Pulse rate not at slowest rate yet 
+          ++current_pulse_rate;  // pulse one second faster
+        }
+      }
+      else
+        current_pulse_rate = DEFAULT_PULSE_RATE;
+
+  }
+  else
+  {
+    // ON_OFF_PIN must have fired
+    if(digitalRead(ON_OFF_PIN) == LOW)
+    {
+      if(current_on_off_state == POWER_OFF)
+      {
+        // Power turned ON
+        current_pulse_rate = DEFAULT_PULSE_RATE;
+        //last_trigger = 3;
+        current_on_off_state = POWER_ON;  // Power now turned ON
+      }
+      else
+      {
+        // Power now turned OFF
+        current_pulse_rate = DEFAULT_PULSE_RATE;
+        //last_trigger = 0;
+        current_on_off_state = POWER_OFF;  // Power now turned OFF
+      }
+    }
+
+  }
+
+  // Turn LED off at the end
+  digitalWrite(LED_OUT_PIN, LOW);   // turn pin off by making the voltage LOW
+
+}
+
+void coil_activation()
 {
   int i;
-  int off_duration_ms = 5000;
 
-  pulse_drive(PIN_0, 50, 50);
-
+  delay(TIME_BASE_MSEC * current_pulse_rate);   // wait based on current coil activation delay state
+  
   for(i=0; i<5; ++i)
   {
-    pulse_drive(PIN_3, 25, 25);
+    pulse_drive(COIL_OUT_PIN, PULSE_DRIVE_ON_PERIOD_MSEC, PULSE_DRIVE_OFF_PERIOD_MSEC);
   }
 
-  delay(off_duration_ms);   // wait
-
 }
-
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(PIN_0, OUTPUT);
-  pinMode(PIN_3, OUTPUT);
+
+  // windless_chime_setup();
+  pin_interrupt_test_setup();
+
+  delay(250);   // wait
+  current_pulse_rate = DEFAULT_PULSE_RATE;
+  current_on_off_state = POWER_OFF;
+  digitalWrite(LED_OUT_PIN, LOW);
+  digitalWrite(COIL_OUT_PIN, LOW);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  
-  //pulse_sequence_feature();
 
-  windless_chime_feature();
+void loop() {
+
+  if(current_on_off_state == POWER_ON)
+    coil_activation();  // main functional routine
 
 }
